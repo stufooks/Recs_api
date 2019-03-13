3d86a4ea34b4c33ea043f248a8092475fb70d8be
Stuart Fooks <sb.fooks@gmail.com>
2019-03-13 16:25:34 -0400

    set up song list view

--- recs/views.py
+++ recs/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.http import JsonResponse
 
+from .models import Song
 # Create your views here.
+
+def song_list(request):
+    songs = Song.objects.all()
+    songs_list = list(songs)
+    return JsonResponse(songs_list, safe=False)
