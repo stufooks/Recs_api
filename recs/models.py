5852ebcf8d65076412ffb6e24b5b48f2169d5b95
Stuart Fooks <sb.fooks@gmail.com>
2019-03-13 16:30:27 -0400

    set up first url and add createdAt to model

--- recs/models.py
+++ recs/models.py
@@ -4,3 +4,4 @@ from django.db import models
 class Song(models.Model):
     track = models.CharField(max_length=200)
     artist = models.CharField(max_length=200)
+    created_at = models.DateTimeField(auto_now_add=True)
