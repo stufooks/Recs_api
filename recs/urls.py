a109896b972617a71ec1950c14c12d1f73c5a879
Stuart Fooks <sb.fooks@gmail.com>
2019-03-14 09:40:33 -0400

    songs create method working

--- recs/urls.py
+++ recs/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.song_list)
+    path('songs', views.song_list),
+    path('songs/new', views.song_create)
 ]
--- recs/views.py
+++ recs/views.py
@@ -1,9 +1,16 @@
 from django.http import JsonResponse
+import json
 
 from .models import Song
 # Create your views here.
 
 def song_list(request):
-    songs = Song.objects.all()
+    songs = Song.objects.all().values()
     songs_list = list(songs)
     return JsonResponse(songs_list, safe=False)
+
+def song_create(request):
+    if request.method == "POST":
+        data = json.loads(request.body)
+        Song.objects.create(track=data['track'], artist=data['artist'])
+        return JsonResponse("create successful", safe=False)
