90a266f0dfb0bf843cf5359d1aa36002cd8e3cb8
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:39:12 -0400

    setting debug back

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['https://evening-reef-23065.herokuapp.com/']
 
