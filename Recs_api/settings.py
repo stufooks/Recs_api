ee6e30d57be7c23477250b0ab05f7f3f7e0223ce
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:12:42 -0400

    trying to deploy 8?

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from os import environ
+import dj-database-url
+
+db_from_env = dj_database_url.config()
+DATABASES[‘default’].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -121,5 +125,4 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATIC_URL = '/static/'
