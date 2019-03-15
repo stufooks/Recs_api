97a8da0bb83f521229bc8368bda148303672daff
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:58:20 -0400

    fixing allowed hosts

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['52.4.95.228']
+ALLOWED_HOSTS = ['.herokuapp.com']
 
 
 # Application definition
