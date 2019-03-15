04f4e225d5463a757603af8dd5e1117d1181d391
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:42:09 -0400

    fixing allowed hosts

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = environ.get('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['evening-reef-23065.herokuapp.com/']
+ALLOWED_HOSTS = ['evening-reef-23065.herokuapp.com/', 'localhost']
 
 
 # Application definition
