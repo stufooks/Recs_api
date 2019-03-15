8c61c1363a57d21212ae1f78a520e91ea89d058a
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:28:50 -0400

    trying to deploy

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -14,8 +14,6 @@ import os
 from os import environ
 import dj_database_url
 
-db_from_env = dj_database_url.config()
-DATABASES['default'].update(db_from_env)
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -88,7 +86,7 @@ DATABASES = {
         'HOST': 'localhost'
     }
 }
-# DATABASES['default'].update(dj_database_url.config())
+DATABASES['default'].update(dj_database_url.config())
 
 
 # Password validation
