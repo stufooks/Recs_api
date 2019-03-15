81f51ad92f24bfbf7c785dbe17b1124b7d593f92
Stuart Fooks <sb.fooks@gmail.com>
2019-03-15 10:04:08 -0400

    trying to deploy 5

--- Recs_api/settings.py
+++ Recs_api/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    # 'django.contrib.staticfiles',
+    'django.contrib.staticfiles',
     'recs'
 ]
 
@@ -121,5 +121,5 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, ‘staticfiles’)
 STATIC_URL = '/static/'
